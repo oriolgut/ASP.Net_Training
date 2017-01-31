9cd738b99a26fe2ff515dc9805aa72d9187ff102
OriolG <download.spam.net@gmail.com>
2017-01-31 12:11:57 +0100

    added openweathermap api call to receive weather data

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -6,6 +7,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Script.Serialization;
 using WebApplication1.Models;
+using WebApplication1.ViewModels.Home;
 
 namespace WebApplication1.Controllers
 {
@@ -14,14 +16,18 @@ namespace WebApplication1.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
-        }
+            WeatherViewModel weather = new WeatherViewModel();
+            OpenWeatherMap.Root weatherData;
+            using (WebClient wc = new WebClient())
+            {
+                var json = wc.DownloadString(@"http:\\api.openweathermap.org/data/2.5/weather?q=Zurich&APPID=69015e4f8b7ba2ddb0cafcae485d3848&units=metric&mode=json");
+                weatherData = JsonConvert.DeserializeObject<OpenWeatherMap.Root>(json);
+            }
+            weather.CityName = weatherData.Name;
+            weather.Temperature = Convert.ToString(weatherData.Main.temp);
+            weather.Humidity = Convert.ToString(weatherData.Main.humidity);
 
-        public JsonResult GetWeather()
-        {
-            Weather weather = new Weather();
-            object content = weather.GetWeahterForecast();
-            return Json(content, JsonRequestBehavior.AllowGet);
+            return View(weather);
         }
     }
 }
---
+++ WebApplication1/ViewModels/Home/WeatherViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+using WebApplication1.Models;
+
+namespace WebApplication1.ViewModels.Home
+{
+    public class WeatherViewModel
+    {
+        public string CityName { get; set; }
+        public string Temperature { get; set; }
+        public string Humidity { get; set; }
+
+    }
+}
