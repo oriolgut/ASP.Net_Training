0107b911f5a2784b354ef3df678dfc610f636a52
OriolG <download.spam.net@gmail.com>
2017-04-05 15:43:37 +0200

    added seperate controller for the openweathermap api call

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -14,28 +15,13 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        private OpenWeatherMap.Root _weatherData;
-
+        
         // GET: Home
         public ActionResult Index()
         {
-            WeatherViewModel weather = new WeatherViewModel();
-            GetWeather();
-            weather.Icon = _weatherData.Weather.First().icon;
-            weather.IconTitle = _weatherData.Weather.First().description;
-            weather.CityName = _weatherData.Name;
-            weather.Temperature = Convert.ToString(_weatherData.Main.temp);
-            weather.Humidity = Convert.ToString(_weatherData.Main.humidity);
-            return View(weather);
+            return View();
         }
 
-        private void GetWeather()
-        {
-            using (WebClient wc = new WebClient())
-            {
-                var json = wc.DownloadString(@"http://api.openweathermap.org/data/2.5/weather?q=Zurich&APPID=69015e4f8b7ba2ddb0cafcae485d3848&units=metric&mode=json");
-                _weatherData = JsonConvert.DeserializeObject<OpenWeatherMap.Root>(json);
-            }
-        }
+
     }
 }
---
+++ WebApplication1/Controllers/WeatherController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using WebApplication1.Models;
+using WebApplication1.ViewModels.Home;
+
+namespace WebApplication1.Controllers
+{
+    public class WeatherController : Controller
+    {
+        // GET: Weather
+        public ActionResult Index()
+        {
+            var weatherData = GetWeather();
+            var weatherInformation = weatherData.Weather.FirstOrDefault();
+            WeatherViewModel weather = new WeatherViewModel
+            {
+                Icon = weatherInformation?.icon,
+                IconTitle = weatherInformation?.description,
+                CityName = weatherData.Name,
+                Temperature = Convert.ToString(weatherData.Main.temp, CultureInfo.InvariantCulture),
+                Humidity = Convert.ToString(weatherData.Main.humidity)
+            };
+            return View(weather);
+        }
+
+        private OpenWeatherMap.Root GetWeather()
+        {
+            using (var wc = new WebClient())
+            {
+                var json = wc.DownloadString(@"http://api.openweathermap.org/data/2.5/weather?q=Zurich&APPID=69015e4f8b7ba2ddb0cafcae485d3848&units=metric&mode=json");
+                return JsonConvert.DeserializeObject<OpenWeatherMap.Root>(json);
+            }
+        }
+    }
+}
